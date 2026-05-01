82beafe84359a636ac934497ebe342f2500d8f98
zhansaya <zansaaerkenova@gmail.com>
2026-05-01 17:56:31 +0500

    feat(sorter):

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,2 +1,21 @@
+import java.util.Random;
+
 public class Sorter {
+
+
+    public void basicSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
